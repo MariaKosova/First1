46c090d8ac4bca3e99306cf73527b019424f6ea8
Maria <kossovamaria@hotmail.com>
2023-12-09 19:30:29 +0300

    Voter

---
+++ src/voters/Voter.java
@@ -0,0 +1,32 @@
+package voters;
+
+public class Voter {
+    private String name;
+    private String address;
+    private int age;
+   /* public Voter (String name, String address, int age) {
+        this.name = name;
+        this.address = address;
+        this.age = age;
+        if (age < 16)
+            System.out.println(" The voter is too young!");
+    }
+
+        public String getName() {
+            return name;
+        }
+
+            public String getAddress () {
+                return getAddress;
+            }
+
+        public int getAge() {
+                return age;
+
+    */
+
+
+
+        }
+
+
