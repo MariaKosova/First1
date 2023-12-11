b944ff28217d50031b486cafa4a88d62ad2a524a
MariaKosova <kossovamaria@hotmail.com>
2023-12-12 01:09:58 +0200

    calculatorMain_homework1112

---
+++ src/calculator_homework1112/Main.java
@@ -0,0 +1,13 @@
+package calculator_homework1112;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator(3,6);
+        Calculator calculator1 = new Calculator(4,5,9);
+        Calculator.findSum(3,6);
+        Calculator.findSum2(4,5,9);
+        Calculator.findDifference(7, 8);
+        Calculator.findDifference2(7,5,2);
+    }
+
+}
