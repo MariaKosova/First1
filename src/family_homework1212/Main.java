c82c67d8dd111e0ce4b4a2266b4fd90e72ce8cb0
MariaKosova <kossovamaria@hotmail.com>
2023-12-13 10:42:53 +0200

    FamilyMain_homework1212

---
+++ src/family_homework1212/Main.java
@@ -0,0 +1,15 @@
+package family_homework1212;
+
+public class Main {
+    public static void main(String[] args) {
+        Family family = new Family();
+        Father father = new Father();
+        Mother mother = new Mother();
+        family.live();
+        father.live();
+        mother.live();
+        //father.surname.name;
+        //father.live();
+    }
+}
+
