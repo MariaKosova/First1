c4b631bc6ac7e0bfeb62596341501ccbb45a3be8
MariaKosova <kossovamaria@hotmail.com>
2023-12-13 10:44:27 +0200

    Family_homework1212

---
+++ src/family_homework1212/Family.java
@@ -0,0 +1,16 @@
+package family_homework1212;
+
+public class Family {
+    String surname = "Smith";
+    String name = "Paul";
+    //String name1 = "Mary";
+
+   // Family (String surname, String name ) {
+    //    this.surname = surname;
+      //  this.name = name;
+ //   }
+
+    void live (){
+        System.out.println ( surname + " family live in London" );
+    }
+}
