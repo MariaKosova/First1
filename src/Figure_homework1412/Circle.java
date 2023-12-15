c9efc82f12bd992882fcf51609b0dc8c04e796ec
Maria <kossovamaria@hotmail.com>
2023-12-15 11:12:47 +0300

    CircleFigure_homework1412

---
+++ src/Figure_homework1412/Circle.java
@@ -0,0 +1,24 @@
+package Figure_homework1412;
+
+public class Circle extends Figure {
+
+    public Circle (double r, double p) {
+        super (r,p);
+    }
+
+
+    @Override
+    public void circleArea() {
+        super.circleArea();
+    }
+
+    public  String toString (){
+        return "Figure {" +
+                "r=" + r +
+                ", p=" + p +
+                '}';
+    }
+
+    //}
+
+}
